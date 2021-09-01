e592e373079cdc516cf363b75546735108d03eda
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-09-01 13:23:34 -0300

    feat:finalizado

---
+++ src/App.js
@@ -0,0 +1,24 @@
+//import logo from './logo.svg';
+import './App.css';
+import CenteredCard from './components/CenteredCard/CenteredCard';
+
+function App() {
+
+  const childrens = [1, 2, 3]
+
+  return (
+    <div className="App">
+      <div className="App-header">
+
+        {
+          childrens.map((item)=>(
+            <CenteredCard children = {item}/>
+          ))
+        }
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CenteredCard/CenteredCard.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+function CenteredCard ({children}){
+    return(
+        <div className="childrenCard">
+            <span>Children {children}</span>
+        </div>
+    )
+}
+
+export default CenteredCard
+
+    
